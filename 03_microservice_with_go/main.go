1019e111a532b75abedae394926568e2d34700f5
ds7cw <delyan.shivachev@hotmail.com>
2024-08-02 07:05:04 +0100

    Microservice with Go Part 2

--- 03_microservice_with_go/main.go
+++ 03_microservice_with_go/main.go
@@ -1,43 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/ds7cw/go_fundamentals/03_microservice_with_go/application"
 )
 
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	// if r.Method == http.MethodGet {
-	// 	// Handle Get
-	// 	if r.URL.Path == "/foo" {
-	// 		// Handle get foo
-	// 	}
-	// }
-
-	// if r.Method == http.MethodPost {
-	// 	// Handle POST
-	// }
-
-	w.Write([]byte("Hello, world!"))
-}
-
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
+	app := application.New()
 
-	router.Get("/hello", basicHandler)
-
-	// use 'curl -X POST localhost:3000/hello -v'
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("Failed to listen to server", err)
+		fmt.Println("failed to start  app:", err)
 	}
+	// use 'curl -X POST localhost:3000/hello -v'
+	// 'curl localhost:3000/orders' - GET
+	// 'curl -X POST localhost:3000/orders' - POST
+
 }
