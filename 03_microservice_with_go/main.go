1c53e39380c766c3072b4118c0518f5d47d94031
ds7cw <delyan.shivachev@hotmail.com>
2024-08-04 21:25:10 +0100

    Microservice with Go Part 3

--- 03_microservice_with_go/main.go
+++ 03_microservice_with_go/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/ds7cw/go_fundamentals/03_microservice_with_go/application"
 )
@@ -10,12 +12,24 @@ import (
 func main() {
 	app := application.New()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	// cancel called at the end of the main func execution
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start  app:", err)
 	}
 	// use 'curl -X POST localhost:3000/hello -v'
 	// 'curl localhost:3000/orders' - GET
 	// 'curl -X POST localhost:3000/orders' - POST
+
+	// Error Message when redis server is not running:
+	//  failed to start  app: failed to connect to redis:
+	//  No connection could be made because the target machine
+	//  actively refused it.
+
+	// redis-cli/ redis-cli shutdown
+	// sudo service redis-server stop/ start/ restart
 
 }
