2c784f2f377730c21e007136df7c10ce4b0045fa
ds7cw <delyan.shivachev@hotmail.com>
2024-10-15 21:49:30 +0100

    Add http server & request handler func

--- 06_go_cards_game/main.go
+++ 06_go_cards_game/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"slices"
 )
 
@@ -46,4 +47,34 @@ func main() {
 			"Cards:", cpd.handData.playerHand,
 		)
 	}
+
+	http.HandleFunc("/new-game", gameHandleFunc)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func gameHandleFunc(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "Welcome to the Poker Simulator!")
+	cards := newDeck()
+	cards.shuffle()
+
+	playersCount := getPlayerCount()
+	playersSlice := createPlayers(playersCount)
+
+	playersSlice, cards = dealToPlayers(playersSlice, cards)
+	flop, cards := dealFlop(cards)
+	turn, cards := dealTurn(cards)
+	river, _ := dealRiver(cards)
+	communityCards := slices.Concat(flop, turn, river)
+
+	for i, pc := range playersSlice {
+		currentPlayerCards := slices.Concat(communityCards, pc.startingHand)
+		playersSlice[i].handData = currentPlayerCards.evaluateHand()
+		fmt.Println("Player", playersSlice[i].playerId,
+			"ComboID:", playersSlice[i].handData.combinationId,
+			"Hand:", playersSlice[i].handData.playerHand)
+	}
+
+	fmt.Fprintf(w, "Community Cardz:\n%q\nPlayers:\n%q",
+		communityCards, playersSlice)
 }
